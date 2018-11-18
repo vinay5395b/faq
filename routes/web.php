b31be3032c0385eff2c4e4f45d9b9fb3bd1240f0
Vinay <vinay5395b@gmail.com>
2018-11-18 00:30:52 -0500

    Added: Answer and view

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 
+Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
+
+Route::resources([
+    'question' => 'QuestionController',
+]);

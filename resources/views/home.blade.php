45289a12d65309d1ca8b92f9b35c7a33d4d8edcb
Vinay <vinay5395b@gmail.com>
2018-11-22 04:42:20 -0500

    Added: Create, edit and delete options for questions and answers.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,9 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
+                        <a class="btn btn-primary float-right" href="{{ route('questions.create') }}">
+                            Create a question
+                        </a>
                         {{--<a class="btn btn-primary float-right" href="#">
                             Create a Question
                         </a>--}}
@@ -29,7 +32,7 @@
                                             <div class="card-footer">
                                                 <p class="card-text">
 
-                                                    <a class="btn btn-primary float-right" href="{{ route('question.show', ['id' => $question->id]) }}">
+                                                    <a class="btn btn-primary float-right" href="{{ route('questions.show', ['id' => $question->id]) }}">
                                                         View
                                                     </a>
                                                 </p>
@@ -37,7 +40,8 @@
                                         </div>
                                     </div>
                                 @empty
-                                    There are no questions to view, you can  create a question.
+                                    There are no questions, you can create one.
+
                                 @endforelse
 
 
